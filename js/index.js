9a763e75a472071efdb413f4538c07213c09f8ab
Mauro Caceres Damore <mauro.ocd@gmail.com>
2023-04-27 00:23:15 -0300

    Separacion de datos de curso

---
+++ js/index.js
@@ -0,0 +1,20 @@
+import {cursos} from './listados-de-cursos.js';
+
+
+let tableBody = document.getElementById('cursos-lista');
+
+
+for (let i = 0; i < cursos.length; i++) {
+    let visita = cursos[i].Visita
+    if (visita > 2) {
+        let a = `<a href="#">`
+        let Curso = `<h3>${cursos[i].Curso}</h3>`;
+        let Imagen = `<p type="button" onclick="cambiar('${cursos[i].Descripcion}')"><img src="${cursos[i].Imagen}"></p>`;
+        let Descripcion = `<p>${cursos[i].Descripcion}</p>`;
+        //let boton = `<p type="button" onclick="cambiar('${cursos[i].Descripcion}')"><img src="${cursos[i].Imagen}"></p>`
+        let af = `</a>`;
+        tableBody.innerHTML += `<li>${ a +  Curso + Imagen + Descripcion + af}</li>`;
+    } 
+};
+
+
---
+++ js/listados-de-cursos.js
@@ -0,0 +1,20 @@
+var cursos = [
+    {Curso: 'Curso de Dibujo tecnico 1',
+    Imagen: 'img/dibujo-tecnico.jpg',
+    Descripcion: 'Introduccion a dibujo tecnico',
+    Visita: 1},
+    {Curso: 'Fisica',
+    Imagen: 'img/fisica.jpg',
+    Descripcion: 'Introduccion Fisica',
+    Visita: 3},
+    {Curso: 'Matematica I',
+    Imagen: 'img/matematica.jpg',
+    Descripcion: 'Introduccion Matematica',
+    Visita: 10},
+    {Curso: 'Quimica',
+    Imagen: 'img/quimica.jpg',
+    Descripcion: 'Introduccion Quimica',
+    Visita: 3}
+    ]
+
+export var cursos 

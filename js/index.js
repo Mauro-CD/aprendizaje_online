e810dd8996f6c04e5413d98e7106d27044821bf1
Mauro Caceres Damore <mauro.ocd@gmail.com>
2023-05-01 14:52:26 -0300

    generacion de json

--- js/index.js
+++ js/index.js
@@ -1,17 +1,27 @@
-import {cursos} from './listados-de-cursos.js';
-
-
 let listado = document.getElementById('cursos-lista');
 
-for (let i = 0; i < cursos.length; i++) {
-    let visita = cursos[i].Visita
-    if (visita > 2) {
-        let a = `<a href="#">`
-        let Curso = `<h3>${cursos[i].Curso}</h3>`;
-        let Imagen = `<p type="button" onclick="cambiar('${cursos[i].Descripcion}')"><img src="${cursos[i].Imagen}"></p>`;
-        let Descripcion = `<p>${cursos[i].Descripcion}</p>`;
-        //let boton = `<p type="button" onclick="cambiar('${cursos[i].Descripcion}')"><img src="${cursos[i].Imagen}"></p>`
-        let af = `</a>`;
-        listado.innerHTML += `<li>${ a +  Curso + Imagen + Descripcion + af}</li>`;
-    } 
-};
+//fetch('http://127.0.0.1:5500/cursos.json', {
+fetch('/json/cursos.json', {
+    method: 'GET',
+    headers: {
+        'Accept': 'application/json',
+    },
+})
+   .then(response => response.json())
+   .then(response => loadCourse(response))
+   
+
+function loadCourse(cursos) {
+    for (let i = 0; i < cursos.length; i++) {
+        let visitas = cursos[i].views
+        if (visitas > 2) {
+            let a = `<a href="#">`
+            let name = `<h3>${cursos[i].name}</h3>`;
+            let image = `<p type="button" onclick="cambiar('${cursos[i].description}')"><img src="${cursos[i].image}"></p>`;
+            let description = `<p>${cursos[i].description}</p>`;
+            //let boton = `<p type="button" onclick="cambiar('${cursos[i].Descripcion}')"><img src="${cursos[i].Imagen}"></p>`
+            let af = `</a>`;
+            listado.innerHTML += `<li>${ a +  name + image + description + af}</li>`;
+        } 
+    };
+}

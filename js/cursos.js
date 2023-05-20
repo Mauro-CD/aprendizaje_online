c4df17d8ccf7a535d82b14a6e5680e22415688f1
Mauro Caceres Damore <mauro.ocd@gmail.com>
2023-05-19 23:23:14 -0300

    Generacion del sitio Cruso con grid

--- js/cursos.js
+++ js/cursos.js
@@ -15,13 +15,13 @@ function loadCourse(cursos) {
     for (let i = 0; i < cursos.length; i++) {
         let visitas = cursos[i].views
         if (visitas > 2) {
-            let a = `<a href="#">`
-            let name = `<h3 id=nameCurso${i} >${cursos[i].name}</h3>`;
-            let image = `<p id=image type="button" onclick="getCourse('${cursos[i].id}')"><img id=imageList src="${cursos[i].image}"></p>`;
+            let a = `<a>`
+            let name = `<h3 id=nameCurso${i} class="title-course">${cursos[i].name}</h3>`;
+            let image = `<p id=image type="button" ><img id=imageList onclick="getCourse('${cursos[i].id}')" src="${cursos[i].image}"></p>`;
             let description = `<p id=description>${cursos[i].description}</p>`;
             //let boton = `<p type="button" onclick="cambiar('${cursos[i].Descripcion}')"><img src="${cursos[i].Imagen}"></p>`
             let af = `</a>`;
-            listado.innerHTML += `<li>${ a +  name + image + description + af}</li>`;
+            listado.innerHTML += `<li >${ a +  name + image + description + af}</li>`;
         } 
     };
 }

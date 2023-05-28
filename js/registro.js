50bf293b2e0f3bf99331d6267e8f8932a662711c
Mauro Caceres Damore <mauro.ocd@gmail.com>
2023-05-28 16:33:09 -0300

    arrglos de conflictos

--- js/registro.js
+++ js/registro.js
@@ -6,13 +6,33 @@
 
 function registro() {
     let name = document.getElementById("name").value;
-    let apellido = document.getElementById("lastname").value;
+    let lastname = document.getElementById("lastname").value;
     let email = document.getElementById("email").value;
+    let emailValidate = /[a-zA-Z0-9._%+-]+@[a-z0-9.-]+\.[a-zA-Z]{2,4}/;
     let password = document.getElementById("password").value;
     let password2 = document.getElementById("password2").value;
-    if ( password === password2 ) {
-        alert("clave correcta")
+    if (name.length > 1) {
+        document.getElementById('invalidName').style.display="none";
     } else {
-        alert("La contraseña no coincide")
+        document.getElementById('invalidName').style.display="block";
+        return 1;
+    }
+    if (lastname.length > 1) {
+        document.getElementById('invalidLastname').style.display="none";
+    } else {
+        document.getElementById('invalidLastname').style.display="block";
+        return 1;
+    }
+    if (emailValidate.test(email)) {
+        document.getElementById('invalidMail').style.display="none";
+    } else {
+        document.getElementById('invalidMail').style.display="block";
+        return 1;
+    }
+    if ( password === password2 && password.length > 5 ) {
+        document.getElementById('invalidPassword').style.display="none";
+    } else {
+        document.getElementById('invalidPassword').style.display="block";
+        return 1;
     }
 }

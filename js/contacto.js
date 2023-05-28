10410952d612547754347196fa43d7afeb9d7f09
Mauro Caceres Damore <mauro.ocd@gmail.com>
2023-05-28 20:55:11 -0300

    arreglo de registros

--- js/contacto.js
+++ js/contacto.js
@@ -1,58 +1,45 @@
-function valida(){
-
-
-    
-
-    if (document.contacto.nombre.value.length == 0) {
-      alert("Ingrese su Nombre")
-      document.contacto.nombre.focus()
-      return 0;
-    }
-  
-    
-  
-    if (document.contacto.email.value.length == 0){
-      alert ("Ingrese su correo electrónico")
-      document.contacto.email.focus()
-      return 0;
-    }
-  
-
-    
-
- 
-
-    telefono = document.contacto.telefono.value
-            telefono = validarEntero(telefono)
-            document.contacto.telefono.value = telefono
-            if (telefono == "") {
-                alert("Indique su número de teléfono.")
-                document.contacto.telefono.focus()
-                return 0;
-            }
-   
-    
-  
-    
-  
-  if (document.contacto.message.value.length == 0){
-    alert ("Por favor deje su mensaje")
-    document.contacto.message.focus()
-    return 0;
-
-  }
-   
-  
-    alert("Muchas gracias por contactarse");
-    document.contacto.submit();
+function valida() {
+  let emailValidate = /[a-zA-Z0-9._%+-]+@[a-z0-9.-]+\.[a-zA-Z]{2,4}/;
+  if (document.contacto.name.value.length > 1) {
+    document.getElementById('invalidName').style.display = "none";
+  } else {
+    document.getElementById('invalidName').style.display = "block";
+    return 1;
   }
 
-  function validarEntero(valor) {
-    valor = parseInt(valor)
+  if (emailValidate.test(document.contacto.email.value)) {
+    document.getElementById('invalidMail').style.display = "none";
+  } else {
+    document.getElementById('invalidMail').style.display = "block";
+    return 1;
+  }
 
-    if (isNaN(valor)) {
-        return ""
-    } else {
-        return valor
-    }
+  telefono = document.contacto.telefono.value
+  telefono = validarEntero(telefono)
+  document.contacto.telefono.value = telefono
+  if (telefono > 0) {
+    document.getElementById('invalidPhone').style.display = "none";
+  } else {
+    document.getElementById('invalidPhone').style.display = "block";
+    return 1;
+  }
+
+  if (document.contacto.message.value.length > 0) {
+    document.getElementById('invalidMessage').style.display = "none";
+  } else {
+    document.getElementById('invalidMessage').style.display = "block";
+    return 1;
+  }
+  alert("Muchas gracias por contactarse");
+  document.contacto.submit();
+}
+
+function validarEntero(valor) {
+  valor = parseInt(valor)
+
+  if (isNaN(valor)) {
+    return ""
+  } else {
+    return valor
+  }
 }
